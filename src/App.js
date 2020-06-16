e32d79bca21f7f094b645b79e79142d6dfdaef23
David Temple <dstemple7@gmail.com>
2020-06-16 13:50:13 -0500

    initial skeleton done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {v4 as uuid} from 'uuid'
+import Form from './Form'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
