2b5431e1308bc44672db491b7c3f7f18a1634aac
David Temple <dstemple7@gmail.com>
2020-06-16 14:30:34 -0500

    MVP mostly done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,75 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {v4 as uuid} from 'uuid'
 import Form from './Form'
+import Associate from './TeamMembers'
 import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+const initialTeamList = [
+  {
+      id:uuid(),
+      name: 'Donald T',
+      email: 'cantuseapc@prezzi.gov',
+      role: 'Unemployed (Coming Soon)',
+  },
+]
+
+const initialFormValues = {
+  name: '',
+  mail: '',
+  role: '',
 }
 
-export default App;
+
+export default function App() {
+  const [associates, setAssociate] = useState(initialTeamList)
+  const [error, setError] = useState('')
+  const [formValues, setFormValues] = useState(initialFormValues)
+
+  const onInputChange = evt => {
+    const {name, value} = evt.target
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
+
+  const onSubmit = evt => {
+    evt.preventDefault()
+    if (!formValues.name || !formValues.email || !formValues.role){
+      setError('Deets missing - try again :)')
+      return
+    }
+  
+
+    const newAssociate = {
+      id: uuid(),
+      name: formValues.name,
+      email: formValues.email,
+      role: formValues.role,
+    }
+
+    setAssociate(associates => [newAssociate, ...associates])
+    setFormValues(initialFormValues)
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">Team Builder</header>
+      <Form
+        values={formValues}
+        onInputChange={onInputChange}
+        onSubmit={onSubmit}
+      />
+      <span style={{color:'red'}}>{error}</span>
+      {
+        associates.map(associate => {
+        return (
+          <Associate key={associate.id} details={associate} />
+        )
+      })
+      
+      }
+    </div>
+  )
+}
--- src/Form.js
+++ src/Form.js
@@ -1,5 +1,51 @@
 import React from 'react'
 
 export default function Form(props) {
+    const {
+        values, 
+        onInputChange,
+        onSubmit
+    } = props
     
+    return (
+        <form className='form container' onSubmit={onSubmit}>
+            <div className='form-group submit'>
+                <h2>Add a Team Member</h2>
+                <button>Submit</button>
+            </div>
+
+            <div className='form-group inputs'>
+                <h4>Add Some More Info</h4>
+                <label>Name:&nbsp;
+                    <input
+                        type='text'
+                        name='name'
+                        value={values.name}
+                        onChange={onInputChange}
+                    />
+                </label>
+                <label>Email:&nbsp;
+                    <input 
+                        id='emailInput'
+                        type='email'
+                        name='email'
+                        value={values.email}
+                        onChange={onInputChange}                    
+                    />
+                </label>
+                <label>Role:&nbsp;
+                    <select 
+                        name='role'
+                        value={values.role}
+                        onChange={onInputChange}
+                    >
+                        <option value=''>-- Whattya Do? --</option>
+                        <option value='Chief Spirit Caster'>Chief Spirit Caster</option>
+                        <option value='Backend Imagineer'>Backend Imagineer</option>
+                        <option value='UI Design Wizard'>UI Design Wizard</option>
+                    </select>
+                </label>
+            </div>
+        </form>
+    )
 }
